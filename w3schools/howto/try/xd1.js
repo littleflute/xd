0fa3288ce0fc9c7fb08132b650c7169a7150e201
littleflute <13699175164@sina.cn>
2017-11-14 14:27:46 -0800

    Add files via upload

--- w3schools/howto/try/xd1.js
+++ w3schools/howto/try/xd1.js
@@ -1,19 +1,92 @@
 
 //xd1.js
 var thisFileName = "xd1";
-var thisFileV	 = "v0.0.11";
+var thisFileV	 = "v0.2.16";
 var thisCodeFun		= function(){
-/*
-_begin_
+/* xd1 code:
+_begin_ 
+<!DOCTYPE html>
+<html>
+<title>W3.JS</title>
+<script src="../lib/w3.js"></script>
+<body>
 
+<h2>Filter List</h2>
 
+<p>Search for a name in the input field:</p>
+
+<input oninput="w3.filterHTML('#id01', 'li', this.value)" placeholder="Search for names..">
+
+<ul id="id01">
+  <li>Alfreds Futterkiste</li>
+  <li>Berglunds snabbk?p</li>
+  <li>Centro comercial Moctezuma</li>
+  <li>Ernst Handel</li>
+  <li>FISSA Fabrica Inter. Salchichas S.A.</li>
+  <li>Galer¨ªa del gastr¨®nomo</li>
+  <li>Island Trading</li>
+  <li>K?niglich Essen</li>
+  <li>Laughing Bacchus Wine Cellars</li>
+  <li>Magazzini Alimentari Riuniti</li>
+  <li>North/South</li>
+  <li>Paris sp¨¦cialit¨¦s</li>
+  <li>Rattlesnake Canyon Grocery</li>
+  <li>Simons bistro</li>
+  <li>The Big Cheese</li>
+  <li>Vaffeljernet</li>
+  <li>Wolski Zajazd</li>
+</ul>
+
+</body>
+</html>
+
+ 
 _end_
 */
 };
+var _blXd1 = {};
+_blXd1.name = "_blXd1";
+_blXd1.v = "v0.0.7";
 
-//alert(thisFileV + ":" + thisCodeFun );
+_blXd1.blrF = function(b,v){
+	v.innerHTML = b.id;
+};
+
+var blo = new blClass;
+
  
 var idDiv = "idDiv_"+thisFileName;
-var od = document.getElementById(idDiv);
-od.innerHTML = idDiv; 
-od.innerHTML = thisCodeFun ;
+var od = document.getElementById(idDiv); 
+var blod1 = blo.blDiv(od,"blod1","blod1","gray");
+
+var blt = new blClassTest;	 
+blt.blGo1 = function(p){	
+	p.blPlxUI(thisFileName + "_" + thisFileV,700,50,_blXd1);
+
+};						
+blt.blGo1(blt); 		
+
+
+var taCode = document.createElement("textarea");
+taCode.id 	= "idTaCode";
+taCode.value	= "alert('xd test');";
+
+
+od.appendChild(taCode); 
+
+var divCode = document.createElement("div");
+divCode.id	= "idDivCode";
+divCode.onclick = function(){alert(divCode.id);}
+divCode.innerHTML = thisCodeFun;
+od.appendChild(divCode); 
+
+
+taCode.value = thisCodeFun;
+
+var s0 = taCode.value ;
+var s1 = s0.split("_begin_");
+var s2 = s1[1];
+var s3 = s2.split("_end_");
+var s4 = s3[0]; 
+
+taCode.value = s4;
