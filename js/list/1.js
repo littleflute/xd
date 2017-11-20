f7ee39544bf8e304bc9550047810227d779a82b8
littleflute <13699175164@sina.cn>
2017-11-19 23:20:35 -0800

    Add files via upload

---
+++ js/list/1.js
@@ -0,0 +1,9 @@
+﻿var o = {
+	name:"blMp3FileList1",
+	v:"0.0.2",
+	s1:"https://littleflute.github.io/Elton-John/Captain%20Fantastic%20and%20the%20Brown%20Dirt%20Cowboy/01 Captain Fantastic and the Brown Dirt Cowboy.mp3",
+	s2:"https://littleflute.github.io/Elton-John/Captain%20Fantastic%20and%20the%20Brown%20Dirt%20Cowboy/02 Tower of Babel.mp3",
+	lastEditTime:"2017/11/20 13:41 bjt"	
+};
+
+if(blPlayerObj) blPlayerObj.blCallBackFun(o);
--- js/plx/blPlxMp3Player.js
+++ js/plx/blPlxMp3Player.js
@@ -2,23 +2,176 @@
 // by littleflute
 // 2017/11/5 13:10 bjt
 
+function _blCreatePlayObj(oBoss)
+{ 
+    	var dt = document.createElement("div"); 
+    	var da = document.createElement("div"); 
+	function fnTimer(o){
+		var n = 0;
+		
+		return function(){n++;o.innerHTML = n;};
+	}
+	var t = setInterval(fnTimer(dt), 1000);
+    oBoss.appendChild(dt); 
+    oBoss.appendChild(da); 
+    var a = document.createElement("audio");
+    a.id = "id_audio_1";
+    a.controls = true;
+    a.volume = 0.2;
+    da.appendChild(a); 
+    return a;
+}
+function _blShowListObj2Div(oDivBoss,obj)
+{        
+	var a = null; 
+	var bOld = null;
+        var oBoss = oDivBoss;
+        if(!oBoss) {
+           oBoss = document.createElement("div");
+           oBoss.id = "divBlShowObj";
+           oBoss.style.border = "green 1px solid";
+           document.body.appendChild(oBoss);
+        } 
+        if(!oBoss){
+            alert("boss error!");return;
+        }
+        oBoss.innerHTML = "";
+	a = _blCreatePlayObj(oBoss); 
+	a.src = obj.s1;
+        for(i in obj)
+        { 
+          var b = document.createElement("button");
+          b.id = b.innerHTML = i;
+          if(i[0]=="b"&&i[1]=="l") b.style.backgroundColor = "yellow";
+          oBoss.appendChild(b);
+          var d = document.createElement("div");
+          d.innerHTML = obj[i];
+          d.style.border = "blue 1px solid";
+          d.style.backgroundColor = "green";
+          d.style.color = "yellow";
+          oBoss.appendChild(d);
+
+          if(i[0]=="s"){ 
+		b.style.backgroundColor = "red";
+		d.innerHTML 	= ""; 
+		d.bShow		= false;
+		b.onclick = function(i_,b_,v_){
+			return function(){
+				a.src = obj[i_]; a.play();
+				b_.style.backgroundColor = "yellow"; 
+			} 
+		}(i,b,d);
+	  }
+        }
+    }    
+function _uiDivMovable(id,x,y){		 					 
+		var idBody = id;							 
+		var idHead = id + "Header";						 
+		var main = _blDiv(document.body,idBody,"_"+id);			 
+		var style ="position: absolute;";			 
+		style += "z-index: 9;";			 
+		style += "background-color: #f1f1f1;";			 
+		style += "text-align: center;";						 
+		style += "border: 1px solid #d3d3d3;";			 
+		style += "left: 400px";			 
+		style += "top: 140px";	 
+		main.setAttribute("style", style);		 
+		var title = _blDiv(main,idHead,"========");			 
+		style ="padding: 10px;";			 
+		style += "z-index: 10;";			 
+		style += "cursor: move;";			 
+		style += "text-align: center;";			 
+		style += "border: 1px solid #fff;";			 
+		style += "background-color: #2196F3;";		 	
+		title.setAttribute("style", style);				 
+		_blMakeDivMovable(main);				 
+		main.style.left 	= x +"px";			 
+		main.style.top		= y +"px";			 
+		var mi1 = _blDiv(main,id+"mi1",id+"mi1");			 
+		mi1.style.textAlign = "left";					 
+		var mi2 = _blDiv(main,id+"mi2",id+"mi2");			 
+		mi2.style.textAlign = "left";					 
+		var mi3 = _blDiv(main,id+"mi3",id+"mi3");			 
+		return main;							 
+	}
+_blhCreateBoard2ShowList 	= function (id,x,y,o2Show){	
+	var o = o2Show; 			 
+		var main = _uiDivMovable(id,x,y);					 
+		var mi1 = _blDiv(main,id+"mi1",id+"mi1");				 
+		var mi1bOnOff 	= _blBtn ( mi1,id+"mi1bOnOff","*");
+		mi1bOnOff.style.float = "right"; 	 
+		mi1bOnOff.bOnOff = "+";							
+		mi1bOnOff.onclick = function (){  						 
+			var mi2 = _blDiv(main,id+"mi2",id+"mi2");			 
+			var oShow = _blDiv(mi2,id+"oShow",id+"oShow");			 
+			if("+"==mi1bOnOff.bOnOff)					 
+			{								 
+					mi1bOnOff.style.backgroundColor = "red";	 
+					mi1bOnOff.bOnOff = "-";				 
+					oShow.style.display = "block";			 
+					_blShowListObj2Div(oShow,o2Show);			 
+			}								 
+			else								 
+			{								 
+					mi1bOnOff.style.backgroundColor = "green";	 
+					mi1bOnOff.bOnOff = "+";				 
+					oShow.style.display = "none";			 
+			}								  
+		}				 
+		var mi3 = _blDiv(main,id+"mi3",id+"mi3");	 						 
+	}
 function blPlxMp3PlayerClass(this_){
-	var f = "blPlxMp3Player"; //************************************ file name ***************
-	var v = "v0.0.37";
+	var fileName = "blPlxMp3Player"; //************************************ file name ***************
+	var v = "v0.0.56";
 	var blo = new blClass;
 	var p = document.getElementById("id_div_LoadPlx_blPlxMp3Player"); //*** parent div id ***
 	var html = "blPlxMp3Player.js - " + v + " is loaded OK!";
 	blo.blDiv(p,"id_div_blPlxMp3Player_1",html); //************************* my debug div id ***
-	var id = "id_board_" + f; //************************************ my board id  ***************
-	blo.blhCreateBoard2ShowObj(id,500,200,this_);
-	this.blrAboutMe = function(b,d){//test
+	var id = "id_board_" + fileName; //************************************ my board id  ***************
+	var l = null;
+	var lo = [];
+	var timer = null; 
+	blo.blhCreateBoard2ShowObj(id,500,200,this);
+	var divMySrcLink = function (oParent){
+		var s = document.baseURI;
+		var path = s.slice(0,1+s.lastIndexOf("/"));	 
+		var nXd = path.lastIndexOf("/xd/");
+		if(nXd!=-1){ 
+			var mySrcPath = path + "js/plx/blPlxMp3Player.js";
+		}
+		else{ 
+			var mySrcPath = path + "https://littleflute.github.io/xd/js/plx/blPlxMp3Player.js";		
+		}
+		var html = "<a target='_blank' href='" + mySrcPath + "' style='color:purple;background-color:skyblue;'>src</a>";
+		var mySrc = blo.blDiv(oParent,"id_blPlxMp3Player_file_src",html,"silver");
+	};
+	
+	var loadListFun = function (id,src){
+		var mySrc = src; 
+		 return function(){ 
+			oScriptList = blo.blScript("id_script_"+id,mySrc); 
+		};
+	};
+	var divListBox = function (oParent){
+		 var b1 = blo.blBtn(oParent,"id_Btn_blPlxMp3Player_List_Box_1", "loadList1","silver");
+		 b1.onclick = loadListFun("list_1","C:/Users/13699/xd/js/list/1.js");
+		 var b2 = blo.blBtn(oParent,"id_Btn_blPlxMp3Player_List_Box_2", "loadList2","silver");
+		 b2.onclick = loadListFun("list_2","C:/Users/13699/xd/js/list/2.js");
+		 l = blo.blDiv(oParent,"id_Div_blPlxMp3Player_List_Box", "ListBox","LimeGreen"); 
+	};
+	this.blrPlayer = function(b,d){//test
 		d.innerHTML = v;
+		divMySrcLink (d);
+		divListBox (d);		
 	}
 	this.blrQuit = function(b,d){//test
 		var d0 = d.parentElement.parentElement.parentElement;
 		d.innerHTML = d0.id;
 	}
-	this.blTest = function(){//test
+	this.blrTest = function(b,d){		 
+	}
+	this.blCallBackFun = function(o){
+		_blhCreateBoard2ShowList(o.name,100,200,o);
 	}
 }
 
@@ -88,18 +241,7 @@ var _update = function()
 blPlxMp3Player.blrList = function(btn,div){
 	var blo = new blClass;
 	div.innerHTML = "";
-
-		var s = document.baseURI;
-		var path = s.slice(0,1+s.lastIndexOf("/"));	 
-		var nXd = path.lastIndexOf("/xd/");
-		if(nXd!=-1){ 
-			var mySrcPath = path + "js/plx/blPlxMp3Player.js";
-		}
-		else{ 
-			var mySrcPath = path + "https://littleflute.github.io/xd/js/plx/blPlxMp3Player.js";		
-		}
-		var html = "<a target='_blank' href='" + mySrcPath + "' style='color:purple;background-color:skyblue;'>src</a>";
-		var mySrc = blo.blDiv(div,"id_blPlxMp3Player_file_src",html,"silver");
+ 
 
 	var t = blo.blTextarea(div,"idTaList",blPlxMp3Player.path+"m.js","skyblue");
 	t.style.width = "100%";
@@ -136,4 +278,5 @@ blPlxMp3Player.blrList = function(btn,div){
 	}
 }
   
-new blPlxMp3PlayerClass(blPlxMp3Player);
+var blPlayerObj = new blPlxMp3PlayerClass(blPlxMp3Player);
+if(blPlayerObj) blPlayerObj.blCallBackFun(o);
