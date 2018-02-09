7dc89fb659f513837024a5b777f025a8b1c6f308
littleflute <13699175164@sina.cn>
2018-02-10 06:13:18 +0800

    Add files via upload

---
+++ english/reading/201802/mp3i.bat
@@ -0,0 +1,37 @@
+del mp3list.txt
+ 
+
+setlocal ENABLEDELAYEDEXPANSION
+call set /a x = 0
+
+echo {									>> mp3list.txt
+
+echo     "title":"%1",							>> mp3list.txt
+echo     "artist":"%2", 							>> mp3list.txt
+echo     "url": "https://littleflute.github.io/xd/english/reading/201802",				>> mp3list.txt
+ 
+for /F "delims="  %%a in ('dir /b *.jpg') do (	
+	call set /a x = !x! + 1
+	echo        "pic!x!":"%%a",					>> mp3list.txt
+) 
+
+
+echo     "audioList":[							>> mp3list.txt
+for /F "delims="  %%a in ('dir /b *.mp3') do (	
+	call set /a x = !x! + 1
+	echo         "%%a",						>> mp3list.txt
+)
+echo     ],      							>> mp3list.txt
+
+
+echo     "lrcList":[							>> mp3list.txt
+for /F "delims="  %%a in ('dir /b *.lrc') do (	
+	call set /a x = !x! + 1
+	echo         "%%a",						>> mp3list.txt
+)
+echo     ]      							>> mp3list.txt
+
+echo }									>> mp3list.txt
+
+@echo off  
+echo ^</div^>	 
